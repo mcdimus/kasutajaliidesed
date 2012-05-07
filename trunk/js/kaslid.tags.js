78f79e96e29acefd30f461b91c04dc4acdb1013f
123noob312@gmail.com <123noob312@gmail.com@f79694d0-c075-7517-bcbd-55ffe3562461>
2012-05-07 01:56:12 +0000

    Knockout removed, tags could be partially added

--- trunk/js/kaslid.tags.js
+++ trunk/js/kaslid.tags.js
@@ -1,77 +1,18 @@
 /*
  * All project code connected to tags.
  */
-// Get tags via AJAX
+var tagsArr = [];
+
 $(function() {
+    // Get tags via AJAX
     $.post('php/tags.php', '{ "action" : "get" }',
         function(answer) {
             $ul = $('div.tag-container ul');
             $(answer).each(function(index, element) {
-                $ul.append($('<li><input type="checkbox" class="checkbox-hidden"/><span>' + element + '</span></li>'));
+                tagsArr.push(element);
             });
+            displayTags();
     }, 'json');
-});
-
-// Clear selected tags
-function clearTags() {
-    $('div#todo-for-tags-hidden span').remove();
-    $('div#todo-for-tags span').attr('data-clicked', 'false')
-    .removeClass();
-}
-
-function Tag(name, checked) {
-    return {
-        name : ko.observable(name),
-        isChecked : ko.observable(checked)
-    };
-}
-
-function appViewModel() {
-    var self = this;
-
-    // ======================================================
-    // == tag ViewModel
-    // ======================================================
-    // observable variables
-    self.tags =
-    ko.observableArray([
-        new Tag("school",false),
-        new Tag("birthday",false),
-        new Tag("meeting",false),
-        new Tag("work",false)
-        ]);
-
-    self.newTagName = ko.observable("");
-
-    // functions on observables
-    self.addTag = function () {
-        if (self.newTagName() != "")
-            self.tags.push(new Tag(self.newTagName(), false));
-    };
-
-    // DOES NOT WORK!!!
-    self.addTagOnEnter = function(event) {
-        var keyCode = event.keyCode;
-        if (keyCode == 13) {
-            this.addTag();
-            return false;
-        }
-        return true;
-    }
-
-    self.deleteTags = function() {
-        var selectedTags = $.grep(self.tags(), function(tag) {
-            return (tag.isChecked());
-        });
-
-        $(selectedTags).each( function(index, tag) {
-            self.tags.remove(tag);
-            $('span#todo-tag-' + tag.name()).remove();
-        });
-    };
-}
-
-$(document).ready(function() {
 
     // Shows / hides tag checkboxes
     var $deleteTags = $('button#deletetags');
@@ -84,7 +25,45 @@ $(document).ready(function() {
             $('div.tag-container input[type="checkbox"]').addClass('checkbox-hidden');
         }
     });
-
-    ko.applyBindings(new appViewModel());
-
 });
+
+// Clears selected tags
+function clearTags() {
+    $('div#todo-for-tags span').attr('data-clicked', 'false').removeClass();
+}
+
+// sends new tags via AJAX
+function sendNewTags(tags) {
+    $.each(tagsArr, function(index, element) {
+        var index = tags.indexOf(element);
+        if (index != -1) {
+            tags = tags.substring(0, index) + tags.substring(index + element.length + 1);
+        }
+    });
+
+    if (tags.charAt(tags.length - 1) == " ") {
+        tags = tags.slice(0, -1);
+    }
+
+    $.each(tags.split(" "), function(index, element) {
+        tagsArr.push(element);
+    });
+
+    $.post('php/tags.php', '{ "action" : "add", "tags" : "' + tags + '" }',
+        function(answer) {
+            $('div#phpanswer').text(answer);
+    });
+    
+    displayTags();
+}
+
+function displayTags() {
+    $ul = $('div.tag-container ul'),
+        $todoForTags = $('#todo-for-tags');
+    $ul.html('');
+    $todoForTags.html('');
+    $.each(tagsArr, function(index, element) {
+        $ul.append($('<li><input type="checkbox" class="checkbox-hidden"/><span>' + element + '</span></li>'));
+        $todoForTags.append('<span data-bind="text: name" data-clicked="false" unselectable="on">' + element + '</span>');  
+    });
+}
